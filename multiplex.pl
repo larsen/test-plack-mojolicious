ba6e6f79a932114ef1e54c0fce8f19285e777a15
vagrant <vagrant@precise32.(none)>
2013-11-18 20:26:42 +0000

    slightly simplified

--- multiplex.pl
+++ multiplex.pl
@@ -8,22 +8,19 @@ use AppTest1;
 use AppTest2;
 use Data::Dumper;
 
-# my $app = sub { $psgi->run(@_) };
-# my $app = $server->load_app('./myapp.pl');
-
 my $app1, $app2;
 
 # AppTest1
 { 
   my $server = Mojo::Server::PSGI->new;
-  my $_app = $server->load_app('./app_test1/script/app_test1');
+  $server->load_app('./app_test1/script/app_test1');
   $app1 = sub { $server->run(@_) }
 }
 
 # AppTest2
 { 
   my $server = Mojo::Server::PSGI->new;
-  my $_app = $server->load_app('./app_test2/script/app_test2');
+  $server->load_app('./app_test2/script/app_test2');
   $app2 = sub { $server->run(@_) }
 }
 
